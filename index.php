310f33734f177c08157af1eaf358d69a0871b0f6
dylanstorts <d-storts@onu.edu>
2017-09-30 18:25:25 -0400

    Create index.php

---
+++ index.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        
+        <link rel="stylesheet" type="text/css" href="MainConnectThree.css">
+        
+        <title>Hey There</title>
+    </head>
+    <body>
+        <h1>Connect Three!</h1>
+        
+        <div id ="game_panel">
+            <div class = "row">
+                <div class ="E"></div>
+                <div class ="E"></div>
+                <div class ="E"></div>
+                <div class ="E"></div>
+            </div>
+            <div class = "row">
+                <div class ="E"></div>
+                <div class ="E"></div>
+                <div class ="E"></div>
+                <div class ="E"></div>
+            </div>
+            <div class = "row">
+                <div class ="E"></div>
+                <div class ="E"></div>
+                <div class ="E"></div>
+                <div class ="E"></div>
+            </div>
+            <div class = "row">
+                <div class ="E"></div>
+                <div class ="E"></div>
+                <div class ="E"></div>
+                <div class ="E"></div>
+            </div>
+            
+        </div>
+        
+        <?php
+        
+        ?>
+    </body>
+</html>
